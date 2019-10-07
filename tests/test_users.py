6ddb3cfe48d860decb6b73fd12807d8f60e5ba0d
Frédéric Montet <frederic.montet@vast.ch>
2019-10-07 19:32:47 +0200

    add users

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,13 +2,13 @@ import unittest
 from bbdata import output
 
 
-class TestMe(unittest.TestCase):
+class TestUsers(unittest.TestCase):
 
     def setUp(self) -> None:
         output.login()
 
     def test__get(self):
-        response = output.me.get()
+        response = output.users.get()
         print(response)
 
     def tearDown(self) -> None:
