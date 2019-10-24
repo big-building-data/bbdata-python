60896202d26c8283c13c4fb9ed8b5d7c8e73ae10
Lorenz Rychener <lorenz.rychener@hefr.ch>
2019-10-24 16:58:26 +0200

    fixed version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bbdata-python",
-    version="0.0.4",
+    version="0.2",
     author="Frédéric Montet",
     author_email="frederic.montet@hefr.ch",
     description="A Python wrapper around the BBData APIs",
