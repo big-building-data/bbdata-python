544b0a8f6f09a9e9102490f38dc071a30f41ce2f
Lorenz Rychener <lorenz.rychener@hefr.ch>
2019-10-24 16:10:02 +0200

    to_dataframe; to_csv; to_pickle -> done

--- bbdata/output/test.py
+++ bbdata/output/test.py
@@ -1,13 +1,13 @@
 from bbdata.routes.output import output
-
 if __name__ == '__main__':
     output.login()
 
     test = output.values.get(
-        "10622,10637",
+        "10622,10639",
         "2019-10-22T19:00",
         "2019-10-22T19:01",
-    ).to_dataframe()
+    )
 
-    print(test)
+    df = test.to_dataframe()
 
+    test.to_csv("fuckers")
