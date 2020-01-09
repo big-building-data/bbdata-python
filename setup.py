035e38fabdfc5b3442d85d2bc0346767bcc73f14
Fred Montet <fredmontet@gmail.com>
2020-01-09 10:26:59 +0100

    Add new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bbdata",
-    version="0.3",
+    version="0.4",
     author="Frédéric Montet",
     author_email="frederic.montet@hefr.ch",
     description="A Python wrapper around the BBData APIs",
